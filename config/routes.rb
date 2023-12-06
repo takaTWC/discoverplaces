63ee703a332248792feda04361044d28e88235c4
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-06 09:08:01 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,33 @@
 Rails.application.routes.draw do
-  #管理者側
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
   #顧客側
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  #管理者側
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
   namespace :admin do
-    get 'homes/top'
+    root to: 'homes#top'
+    resources :users, only: [:index, :show, :update]
+    resources :comments, only: [:index, :destroy]
   end
-  namespace :public do
-    get 'homes/top'
+
+  scope module: :public do
+    root to: 'homes#top'
+    get "search" =>"searches#search"
+    get "/about" =>"homes#about"
+    resources :users, only: [:show, :edit, :update] do
+      collection do
+        patch :withdrawal
+      end
+    end
+    resources :posts, except: [:edit, :update]
+    resources :bookmarks, only: [:index, :create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    resources :watch_lists, only: [:index, :create, :destroy]
   end
 end
