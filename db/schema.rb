9a594d73b54104a5c8de5ecd372cb287712aacf5
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-07 14:52:13 +0000

    [Add]ヘッダーフッター

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_05_144535) do
+ActiveRecord::Schema.define(version: 2023_12_07_134236) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 2023_12_05_144535) do
   create_table "tag_relations", force: :cascade do |t|
     t.integer "tag_id", null: false
     t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
