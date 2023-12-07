7331052fe781e0832cdf1840b29df3fea430b4b7
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-07 15:34:53 +0000

    [Add]会員登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  def after_sign_in_path_for(resource)
-    root_path
-  end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :email, :password, :password_confirmation, :name, :introduction, :image]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 end
