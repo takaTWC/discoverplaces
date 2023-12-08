adecd62b26071ca9b732a4616391b388e1bdd270
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-08 06:13:01 +0000

    [Fix]ゲストログイン時に情報編集不可

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,16 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
   end
 end
