00394d56c5e63044907f4b1e0982acd1dbc20ebd
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-09 09:11:54 +0000

    [Add]Adminユーザー

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin_email = ENV['ADMIN_EMAIL']
+admin_password = ENV['ADMIN_PASSWORD']
+
+Admin.create(email: admin_email, password: admin_password)
