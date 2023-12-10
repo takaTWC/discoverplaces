84457ec3e33668c1ef285dbb0daa489d56a02824
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-10 08:06:24 +0000

    [Add]コメント削除機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,7 +6,15 @@ class Public::CommentsController < ApplicationController
     comment.save
     redirect_to request.referer
   end
-  
+
+  def destroy
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.find_by(params[:id])
+    comment.post_id = post.id
+    comment.destroy
+    redirect_to request.referer
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment)
