b72224b96f0b9fcc1da645fe024674ccecb74e94
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-20 13:11:57 +0000

    [Fix]コメント機能非同期化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,18 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    comment = current_user.comments.find_by(params[:id])
-    comment.post_id = post.id
-    comment.destroy
-    redirect_to request.referer
+    @comment = current_user.comments.find_by(params[:id])
+    @comment.post_id = post.id
+    @comment.destroy
   end
 
   def index
