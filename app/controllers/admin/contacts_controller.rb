e4a1339e454ab910a785616562ad405e546e2f63
EC2 Default User <ysk.ymtk0613@gmail.com>
2023-12-22 17:37:42 +0000

    [Add]管理者側問い合わせ表示ページ

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,2 +1,11 @@
 class Admin::ContactsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @contacts = Contact.page(params[:page]).per(10)
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    @user = @contact.user
+  end
 end
