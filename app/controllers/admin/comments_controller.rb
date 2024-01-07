96f2050471ebd57fefc1186ee3fb99b9cb8bd456
EC2 Default User <ysk.ymtk0613@gmail.com>
2024-01-07 08:58:44 +0000

    [Add]管理者側コメント並び替え

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,18 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @comments = Comment.page(params[:page]).per(10)
+    if params[:latest]
+      base_query = Comment.latest
+    elsif params[:old]
+      base_query = Comment.old
+    elsif params[:score_high_count]
+      base_query = Comment.score_high_count
+    elsif params[:score_low_count]
+      base_query = Comment.score_low_count
+    else
+      base_query = Comment.all
+    end
+    @comments = base_query.page(params[:page]).per(10)
   end
 
   def destroy
